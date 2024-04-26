848d05a63fd576f1ecf2f66dafc840a3f78b8d18
rafa_office <central.salas@gmail.com>
2024-04-26 12:21:30 -0500

    Se agregaron nuevos proyectos

--- app/main.py
+++ app/main.py
@@ -1,14 +1,17 @@
 import utils
 import read_csv
 import charts
+import pandas as pd
+
 
 def run():
   data = read_csv.read_csv('data.csv')
-  # Solucion dada por el profesor, se filtra por continente y se ordena por poblacion
-  data = list(filter(lambda item : item['Continent'] == 'South America',data))
 
-  countries = list(map(lambda x: x['Country'], data))
-  percentages = list(map(lambda x: x['World Population Percentage'], data))
+  df = pd.read_csv('data.csv')
+  df = df[df['Continent'] == 'Africa']
+
+  countries = df['Country'].values
+  percentages = df['World Population Percentage'].values
   charts.generate_pie_chart(countries, percentages)
 
   country = input('Type Country => ')
@@ -19,11 +22,27 @@ def run():
     labels, values = utils.get_population(country)
     charts.generate_bar_chart(country['Country'], labels, values)
 
-'''
-def run():
-  labels, values = read_csv.read_world_popu('./app/data.csv')
-  charts.generate_pie_chart(labels, values)
-'''
+# def run():
+#   data = read_csv.read_csv('data.csv')
+#   # Solucion dada por el profesor, se filtra por continente y se ordena por poblacion
+#   data = list(filter(lambda item : item['Continent'] == 'South America',data))
+
+#   countries = list(map(lambda x: x['Country'], data))
+#   percentages = list(map(lambda x: x['World Population Percentage'], data))
+#   charts.generate_pie_chart(countries, percentages)
+
+#   country = input('Type Country => ')
+#   result = utils.population_by_country(data, country)
+
+#   if len(result) > 0:
+#     country = result[0]
+#     labels, values = utils.get_population(country)
+#     charts.generate_bar_chart(country['Country'], labels, values)
+
+# def run():
+#   labels, values = read_csv.read_world_popu('./app/data.csv')
+#   charts.generate_pie_chart(labels, values)
+
 
 if __name__ == '__main__':
   run()
---
+++ web-server/main.py
@@ -0,0 +1,21 @@
+import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get('/')
+def get_list():
+    return [1,2,3]
+
+@app.get('/contact', response_class=HTMLResponse)
+def get_dict():
+    return '''
+        <h1>Hola soy una pagina
+    '''
+
+def run():
+    store.get_categories()
+
+if __name__ == '__main__':
+    run()
